0d665e7e0d466b61fa11c443952c68e2d0192c40
Amira <alvaro.mira.del.amo@gmail.com>
2023-03-14 11:59:33 +0100

    Adding Vuex and initialising it with dummy data

--- una-vida-extra-fe/src/main.js
+++ una-vida-extra-fe/src/main.js
@@ -1,11 +1,22 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex';
 import App from './App.vue'
 import router from './router'
 
 //import base collection of css
 import './assets/main.css'
 
+//vuex store init
+const store = createStore({
+    state() {
+        return {
+            userLoggedIn: false
+        };
+    }
+});
+
 //create the Vue app and mount it
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
