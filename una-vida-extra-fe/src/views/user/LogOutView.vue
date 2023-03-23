2c2ade68d776aeaa8d7335ce6a2caef77b2d8791
Amira <alvaro.mira.del.amo@gmail.com>
2023-03-23 14:26:05 +0100

    Updated basic logic of logout view

--- una-vida-extra-fe/src/views/user/LogOutView.vue
+++ una-vida-extra-fe/src/views/user/LogOutView.vue
@@ -1,38 +1,42 @@
 <script setup>
 import axios from "axios";
 import { useRouter } from "vue-router";
+import { onMounted } from "vue";
 
-await axios.post("http://localhost:8000/api1/logout");
+onMounted(() => {
+  axios.post("http://localhost:8000/api1/logout");
 
-const router = useRouter();
-//using replace to avoid potential issues with back and forth logging in and out
-router.replace({ name: "login" });
+  const router = useRouter();
+  //using replace to avoid potential issues with back and forth logging in and out
+  router.replace({ name: "login" });
+});
+
 /*
 async function logout() {
-  // this.processing = true
+// this.processing = true
 
-  try {
-    const cookie = await axios.get("http://localhost:8000/sanctum/csrf-cookie");
+try {
+  const cookie = await axios.get("http://localhost:8000/sanctum/csrf-cookie");
 
-    const resp = await axios.post("http://localhost:8000/api1/login", {
-      email: data.email.val,
-      password: data.password.val,
-    });
-    console.log(resp);
-    //store.commit("logUserIn");
-    //router.push("/products");
-  } catch (response) {
-    if (response.status === 422) {
-      //this.validationErrors = response.data.errors
-      console.log(response.data.errors);
-    } else {
-      //this.validationErrors = {}
-      //alert(response.data.message)
-      console.log(response);
-    }
-  } finally {
-    console.log("login function over.");
+  const resp = await axios.post("http://localhost:8000/api1/login", {
+    email: data.email.val,
+    password: data.password.val,
+  });
+  console.log(resp);
+  //store.commit("logUserIn");
+  //router.push("/products");
+} catch (response) {
+  if (response.status === 422) {
+    //this.validationErrors = response.data.errors
+    console.log(response.data.errors);
+  } else {
+    //this.validationErrors = {}
+    //alert(response.data.message)
+    console.log(response);
   }
+} finally {
+  console.log("login function over.");
+}
 }*/
 </script>
 
