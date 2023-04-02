1319e461a40a6703a556c68f021b58d2f5980db7
Amira <alvaro.mira.del.amo@gmail.com>
2023-04-02 18:33:46 +0200

    Adding to the logic to perform api user call for user requests specific to the logged in user

--- una-vida-extra-fe/src/views/requests/RequestListView.vue
+++ una-vida-extra-fe/src/views/requests/RequestListView.vue
@@ -1,6 +1,80 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, defineProps, computed, reactive } from "vue";
 import RequestCard from "../../components/ui/request/RequestCard.vue";
+import { useRouter, useRoute } from "vue-router";
+import axios from "axios";
+import { useStore } from "vuex";
+
+const route = useRoute();
+const store = useStore();
+
+const isLoading = ref(false);
+const requestError = ref(false);
+const requestCurrentPage = ref(1);
+const errorDetails = reactive({
+  code: "",
+  message: "",
+  errors: [],
+});
+
+const loggedInUser = computed(() => {
+  return store.state.user.id;
+});
+
+//fetch product requests from the public api
+const getProductRequests = async () => {
+  try {
+    const resp = await axios.get(
+      `http://localhost:8000/api1/users/${loggedInUser.value}/requests`
+    );
+    //console.log(resp);
+    prodRequests.value = resp.data.data;
+    requestCurrentPage.value = resp.data.current_page;
+
+    console.log(resp.data.data);
+
+    console.log(prodRequests);
+    //isLoading.value = false;
+    requestError.value = false;
+    //router.push({ name: "products", query: { registration: "success" } });
+  } catch (error) {
+    // Handle Error Here
+    console.error(error);
+    //isLoading.value = false;
+    requestError.value = true;
+
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Error data", error.response.data);
+      console.error("Error status", error.response.status);
+      errorDetails.code = error.response.status;
+      errorDetails.message = error.message;
+      if (error.response.data.errors) {
+        let requestRecivedErrors = error.response.data.errors;
+        for (const property in requestRecivedErrors) {
+          errorDetails.errors.push(requestRecivedErrors[property].toString());
+        }
+      }
+      //console.log(error.response.headers);
+      // } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser
+      // and an instance of http.ClientRequest in node.js
+      //   console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error message", error.message);
+      console.error("Error code", error.code);
+      errorDetails.code = error.code;
+      errorDetails.message = error.message;
+    }
+  }
+};
+
+getProductRequests();
+
+const prodRequests = ref([]);
 
 const requests = ref([
   {
