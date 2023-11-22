7759bd436645207f55c993cd56dccd68eea3bbb1
Amira <alvaro.mira.del.amo@gmail.com>
2023-11-23 00:33:14 +0100

    Stop using axios to avoid CORS

--- una-vida-extra-fe/src/components/ui/ProfileImage.vue
+++ una-vida-extra-fe/src/components/ui/ProfileImage.vue
@@ -7,18 +7,13 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import md5 from "md5";
-import axios from "axios";
 
 const props = defineProps(["userEmail"]);
 const isGravatarValid = ref(false);
-
-const checkGravatar = async () => {};
 const gravatarUrl = ref("");
 
 onMounted(() => {
   gravatarUrl.value = generateGravatarUrl(props.userEmail);
-  //gravatarUrl.value = generateGravatarUrl("alvaromira83@gmail.com");
-  console.log("Loading user image");
 });
 
 const generateGravatarUrl = async (email) => {
@@ -26,8 +21,30 @@ const generateGravatarUrl = async (email) => {
   console.log(hash);
 
   try {
-    const response = await axios.get(
-      `https://gravatar.com/avatar/${hash}?d=404`
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          // Gravatar exists
+          isGravatarValid.value = true;
+          gravatarUrl.value = `https://gravatar.com/avatar/${hash}?d=identicon`;
+          // document.getElementById('result').innerText = 'Gravatar exists!';
+        } else {
+          isGravatarValid.value = false;
+          gravatarUrl.value = "https://ui-avatars.com/api/?name=" + email;
+          // Gravatar does not exist
+          //document.getElementById('result').innerText = 'Gravatar does not exist.';
+        }
+      }
+    };
+    xhr.open("GET", `https://gravatar.com/avatar/${hash}?d=404`, true);
+    xhr.send();
+
+    /*  const response = await axios.get(
+      `https://gravatar.com/avatar/${hash}?d=404`,
+      {
+        withCredentials: false,
+      }
     );
 
     if (response.status === 200) {
@@ -38,10 +55,10 @@ const generateGravatarUrl = async (email) => {
       // Gravatar does not exist
       isGravatarValid.value = false;
       gravatarUrl.value = "https://ui-avatars.com/api/?name=" + email;
-    }
+    }*/
   } catch (error) {
     // Handle errors
-    console.error("Error checking Gravatar:", error.message);
+    console.error("Error checking the Gravatar:", error.message);
     isGravatarValid.value = false;
     gravatarUrl.value = "https://ui-avatars.com/api/?name=" + email;
   }
