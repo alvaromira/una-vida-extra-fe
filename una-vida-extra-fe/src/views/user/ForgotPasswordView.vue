3bbb14b38f3278984eba8294dbeac95550667e76
Amira <alvaro.mira.del.amo@gmail.com>
2024-02-09 00:06:56 +0100

    Created form for forgot password

---
+++ una-vida-extra-fe/src/views/user/ForgotPasswordView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import UserForgotPasswordForm from "../../components/user/UserForgotPasswordForm.vue";
+</script>
+
+<template>
+  <div class="form-wrapper">
+    <UserForgotPasswordForm />
+  </div>
+</template>
+
+<style scoped>
+.form-wrapper {
+  max-width: 50%;
+}
+</style>
