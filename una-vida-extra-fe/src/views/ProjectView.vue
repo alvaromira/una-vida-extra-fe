fd13fe9756e309ca1e0bbd318195da0cddc9840c
Amira <alvaro.mira.del.amo@gmail.com>
2024-05-15 18:04:47 +0200

    Created sponsors and project views

--- una-vida-extra-fe/src/views/ProjectView.vue
+++ una-vida-extra-fe/src/views/ProjectView.vue
@@ -1,64 +1,119 @@
 <!--Vista donde se muestra el contenido de la pagina del proyecto. ES HTML estatico. -->
-
+<!--Una vista muy sencilla con las instrucciones para ponerse en contacto con los administradores. Inspirada en Yummygum-->
 <template>
-  <div>
-    <h2>This is the project view</h2>
-    <p>
-      Eu sociis mattis lacinia aliquam vel eu nullam nibh praesent. Litora
-      feugiat arcu varius blandit tortor hac commodo magna dictumst! Vulputate
-      mus porttitor dui auctor magnis odio convallis class dolor consectetur. Id
-      vehicula fusce habitant tortor mauris penatibus. Ornare blandit aliquet
-      faucibus risus, arcu venenatis vehicula imperdiet. Sociosqu mollis rhoncus
-      habitasse. Platea commodo imperdiet aenean egestas. Placerat porta
-      interdum ac nec porttitor hac. A dui eleifend torquent semper ut
-      parturient eu. Commodo quis maecenas, eros fames eleifend vehicula.
-      Torquent nisl sodales nam libero nulla sodales, dictumst est nec nisl
-      taciti a. Ultricies ultricies.
-    </p>
-    <p>
-      Metus id et ultricies. Dictumst dignissim venenatis fringilla quam
-      curabitur molestie penatibus fames dolor vitae dictumst vivamus? Augue
-      donec auctor volutpat cras vivamus lobortis mi, vel volutpat. Primis
-      blandit consequat sed sodales laoreet in consequat. Luctus varius nulla
-      nibh duis. Elit taciti leo, est tempor. Cubilia sociosqu arcu sapien
-      nascetur lacinia. Dignissim ante placerat praesent nec? Maecenas dui
-      nascetur nascetur penatibus taciti viverra pharetra elit sed. Suspendisse
-      convallis varius etiam mi sed cursus amet suspendisse, adipiscing dui.
-      Accumsan mollis mattis parturient ligula? Condimentum, purus fames euismod
-      magna potenti massa molestie. Aliquam ac penatibus vel sociosqu augue
-      aliquet posuere pulvinar.
-    </p>
-    <p>
-      Convallis nullam blandit hendrerit tempor sollicitudin aenean netus! Eget
-      magnis tempor metus leo. Ad ad phasellus dictumst vulputate taciti
-      pretium. Integer tempus fames suscipit aenean diam orci arcu dolor
-      interdum. Iaculis nostra lacinia nec mollis eleifend eleifend. Senectus
-      vulputate praesent lorem imperdiet fermentum scelerisque proin curabitur
-      mus ullamcorper torquent. Ornare orci integer tortor vestibulum nisi,
-      varius mi aliquam suspendisse? Massa.
-    </p>
-    <p>
-      Taciti id posuere euismod per, non gravida duis scelerisque? Fermentum
-      vestibulum ultricies, phasellus massa nulla. Auctor duis leo, habitasse
-      quam vel mi arcu non id. Massa nibh turpis consequat habitasse lobortis
-      cras in. Commodo adipiscing libero litora congue sem amet condimentum
-      faucibus egestas taciti. Dis fames placerat cras primis dignissim in ipsum
-      nisl nascetur? Magnis orci donec elit volutpat justo lectus. Suspendisse
-      integer eget nisi sociosqu natoque a class mi. Ligula bibendum lobortis
-      cras tempus accumsan. Adipiscing bibendum gravida lacinia litora hac
-      feugiat. Laoreet velit praesent posuere sapien non malesuada tellus justo
-      sagittis. Primis habitasse proin.
-    </p>
-    <p>
-      Arcu, non magnis felis tempus pharetra luctus in sem pharetra nullam
-      maecenas. Sapien potenti fusce euismod. Cras quisque aliquet, nulla
-      aliquet facilisis orci. Potenti suspendisse at imperdiet penatibus laoreet
-      erat turpis quam habitasse. Duis habitant molestie neque ornare at
-      posuere. Id habitant lacinia fames quis bibendum tellus. Dis, primis
-      pellentesque ridiculus urna. Congue risus aliquam primis vehicula aptent
-      id interdum lacus! Nulla!
-    </p>
+  <div class="row justify-content-md-center">
+    <div class="col-md-12 cta-wrapper">
+      <div class="row">
+        <div class="col-md-6 light-side cta-side">
+          <img
+            src="https://source.unsplash.com/a-book-shelf-filled-with-lots-of-books-Xhqiy7cuwMA"
+            class="img-fluid"
+          />
+        </div>
+        <div class="col-md-6 dark-side cta-side">
+          <h2>Una vida extra</h2>
+          <p>
+            We believe in the power of community, generosity, and
+            sustainability. Our platform is a vibrant web community where people
+            come together to share items they no longer need, but that still
+            have plenty of life left in them. It's all about giving back,
+            reusing, and recycling – and best of all, it's completely free.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 light-side cta-side">
+          <h2>How it works</h2>
+          <p>
+            <strong>Donate</strong>: Have items gathering dust at home? Give
+            them a new lease on life by donating them. Whether it's clothing,
+            books, household items, or anything in between, your donations can
+            make a real difference to someone else.
+          </p>
+
+          <p>
+            <strong>Request</strong>: Need something specific? Browse our
+            listings and make requests for items you're looking for. All
+            requests are sorted by proximity first, ensuring that items are
+            preferably shared locally and sustainably.
+          </p>
+
+          <p>
+            <strong>Pick Up</strong>: Once your request is accepted, simply
+            arrange a time to pick up the item from its original location. It's
+            that easy – no money involved, just a spirit of community and
+            kindness.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 dark-side cta-side">
+          <h2>Our values</h2>
+          <p>
+            <strong>Community</strong>: We're all about bringing people together
+            and fostering a sense of belonging.
+          </p>
+          <p>
+            <strong>Generosity</strong>: Sharing is caring, and our community
+            thrives on the generosity of its members.
+          </p>
+          <p>
+            <strong>Sustainability</strong>: By reusing and recycling items,
+            we're helping to reduce waste and lessen our environmental impact.
+          </p>
+        </div>
+        <div class="col-md-6 light-side cta-side">
+          <img
+            src="https://source.unsplash.com/planet-earth-first-signage-sticked-in-gray-post-outdoors-D0xQQsZovws"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.cta-side {
+  background-color: white;
+  padding: 4rem;
+}
+.cta-wrapper {
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.cta-side h2 {
+  font-size: 4rem;
+  letter-spacing: -0.25rem;
+  padding-bottom: 2rem;
+}
+.cta-side p {
+  color: rgb(33, 51, 67);
+  font-weight: 300;
+}
+.contact-email {
+  text-decoration: none;
+  font-size: 1.25rem;
+  font-weight: 500;
+}
+.contact-email:hover {
+  text-decoration: underline;
+}
 
-<style scoped></style>
+.light-side {
+}
+
+.dark-side {
+  background-color: #7ab370;
+  color: white;
+}
+.dark-side p {
+  color: white;
+}
+.dark-side h2 {
+  color: white;
+}
+.dark-side .contact-email {
+  color: white;
+}
+</style>
--- una-vida-extra-fe/src/views/SponsorsView.vue
+++ una-vida-extra-fe/src/views/SponsorsView.vue
@@ -1,63 +1,89 @@
 <!--Vista donde se muestra el contenido de la pagina de sponsor. ES HTML estatico. -->
 <template>
   <div>
-    <h2>This is the sponsors page</h2>
+    <h2>Our sponsors and supporters</h2>
     <p>
-      Eu sociis mattis lacinia aliquam vel eu nullam nibh praesent. Litora
-      feugiat arcu varius blandit tortor hac commodo magna dictumst! Vulputate
-      mus porttitor dui auctor magnis odio convallis class dolor consectetur. Id
-      vehicula fusce habitant tortor mauris penatibus. Ornare blandit aliquet
-      faucibus risus, arcu venenatis vehicula imperdiet. Sociosqu mollis rhoncus
-      habitasse. Platea commodo imperdiet aenean egestas. Placerat porta
-      interdum ac nec porttitor hac. A dui eleifend torquent semper ut
-      parturient eu. Commodo quis maecenas, eros fames eleifend vehicula.
-      Torquent nisl sodales nam libero nulla sodales, dictumst est nec nisl
-      taciti a. Ultricies ultricies.
+      Join us at <strong>Una vida extra</strong> and be part of something
+      meaningful. Together, let's make a positive impact on our local
+      communities and the planet.
     </p>
     <p>
-      Metus id et ultricies. Dictumst dignissim venenatis fringilla quam
-      curabitur molestie penatibus fames dolor vitae dictumst vivamus? Augue
-      donec auctor volutpat cras vivamus lobortis mi, vel volutpat. Primis
-      blandit consequat sed sodales laoreet in consequat. Luctus varius nulla
-      nibh duis. Elit taciti leo, est tempor. Cubilia sociosqu arcu sapien
-      nascetur lacinia. Dignissim ante placerat praesent nec? Maecenas dui
-      nascetur nascetur penatibus taciti viverra pharetra elit sed. Suspendisse
-      convallis varius etiam mi sed cursus amet suspendisse, adipiscing dui.
-      Accumsan mollis mattis parturient ligula? Condimentum, purus fames euismod
-      magna potenti massa molestie. Aliquam ac penatibus vel sociosqu augue
-      aliquet posuere pulvinar.
+      Are you passionate about community, sustainability, and making a positive
+      impact on the world? Join us at Una vida extra, where we're on a mission
+      to create a more connected, generous, and sustainable society.
     </p>
+
+    <h2>Become a Sponsor of Una vida extra: Make a Difference Today!</h2>
     <p>
-      Convallis nullam blandit hendrerit tempor sollicitudin aenean netus! Eget
-      magnis tempor metus leo. Ad ad phasellus dictumst vulputate taciti
-      pretium. Integer tempus fames suscipit aenean diam orci arcu dolor
-      interdum. Iaculis nostra lacinia nec mollis eleifend eleifend. Senectus
-      vulputate praesent lorem imperdiet fermentum scelerisque proin curabitur
-      mus ullamcorper torquent. Ornare orci integer tortor vestibulum nisi,
-      varius mi aliquam suspendisse? Massa.
+      Are you passionate about community, sustainability, and making a positive
+      impact on the world? Join us at Una vida extra, where we're on a mission
+      to create a more connected, generous, and sustainable society.
     </p>
-    <p>
-      Taciti id posuere euismod per, non gravida duis scelerisque? Fermentum
-      vestibulum ultricies, phasellus massa nulla. Auctor duis leo, habitasse
-      quam vel mi arcu non id. Massa nibh turpis consequat habitasse lobortis
-      cras in. Commodo adipiscing libero litora congue sem amet condimentum
-      faucibus egestas taciti. Dis fames placerat cras primis dignissim in ipsum
-      nisl nascetur? Magnis orci donec elit volutpat justo lectus. Suspendisse
-      integer eget nisi sociosqu natoque a class mi. Ligula bibendum lobortis
-      cras tempus accumsan. Adipiscing bibendum gravida lacinia litora hac
-      feugiat. Laoreet velit praesent posuere sapien non malesuada tellus justo
-      sagittis. Primis habitasse proin.
-    </p>
-    <p>
-      Arcu, non magnis felis tempus pharetra luctus in sem pharetra nullam
-      maecenas. Sapien potenti fusce euismod. Cras quisque aliquet, nulla
-      aliquet facilisis orci. Potenti suspendisse at imperdiet penatibus laoreet
-      erat turpis quam habitasse. Duis habitant molestie neque ornare at
-      posuere. Id habitant lacinia fames quis bibendum tellus. Dis, primis
-      pellentesque ridiculus urna. Congue risus aliquam primis vehicula aptent
-      id interdum lacus! Nulla!
-    </p>
+
+    <div class="sponsor-info">
+      <h2>Why Sponsor Us:</h2>
+      <p>
+        By sponsoring Una vida extra, you'll be supporting a project that
+        embodies values such as community, generosity, and sustainability. Your
+        sponsorship will help us to continue to work on the platform itself,
+        while you contribute to raise awareness about the importance of reuse,
+        recycling, and local community engagement.
+      </p>
+      <p>
+        As a sponsor of Una vida extra, you'll receive recognition and brand
+        visibily.
+      </p>
+    </div>
+
+    <div>
+      <p>
+        For sponsorship inquiries and more information, please contact us at
+        [insert contact email or form link].
+      </p>
+    </div>
+
+    <div class="sponsors-shop-window">
+      <h1>Our Sponsors</h1>
+      <p>
+        At the moment, we do not have any official supporter or sponsor 😔. All
+        our work is privately supported 😅. Our sponsors's info will be
+        displayed below.
+      </p>
+      <div class="row">
+        <div class="col">
+          <div class="card mb-3" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img
+                  src="https://source.unsplash.com/photography-of-cafe-with-led-signage-and-pendant-lamps-and-menu-boards-ItaV89TNkks"
+                  class="img-fluid"
+                  alt="Luna's cafe"
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Luna's cafe</h5>
+                  <p class="card-text">
+                    Luna's is more than just a cafe – it's a gathering place
+                    where locals come together to indulge in the art of
+                    specialty coffee.
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted">luna-cafe.site</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup></script>
-<style scoped></style>
+<style scoped>
+.card {
+  border-radius: 10px;
+  overflow: hidden;
+}
+</style>
